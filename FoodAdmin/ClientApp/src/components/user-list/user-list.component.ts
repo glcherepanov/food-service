54d2aa10b3447510c323da712b9ee3c183138551
Cherepanov Gleb <glcherepanov@yandex.ru>
2018-10-24 10:23:39 +0300

    Добавил объект пользователь и UserDto

--- FoodAdmin/ClientApp/src/components/user-list/user-list.component.ts
+++ FoodAdmin/ClientApp/src/components/user-list/user-list.component.ts
@@ -6,4 +6,17 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './user-list.component.html'
 })
 export class UserListComponent {
+  public users: UserDto[];
+
+  public constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<UserDto[]>(baseUrl + 'api/User/UserDtos').subscribe(result => {
+      this.users = result;
+    }, error => console.error(error));
+  }
 };
+
+interface UserDto{
+  name: string,
+  group: string,
+  team: string
+}
